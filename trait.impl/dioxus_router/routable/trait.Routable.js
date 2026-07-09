811bf9c975253f73ef18406ac7b1b67037af8167
sjkim1127 <freexxx9206@naver.com>
2026-07-09 15:04:48 +0000

    Deploying to gh-pages from @ sjkim1127/Eon@e263c1b7537c300b791389a752787c76ede55246 🚀

--- trait.impl/dioxus_router/routable/trait.Routable.js
+++ trait.impl/dioxus_router/routable/trait.Routable.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["eon_ui",[["impl Routable for <a class=\"enum\" href=\"eon_ui/router/enum.Route.html\" title=\"enum eon_ui::router::Route\">Route</a><div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,</div>",0]]]]);
+    const implementors = Object.fromEntries([["eon_ui",[["impl Routable for <a class=\"enum\" href=\"eon_ui/router/enum.Route.html\" title=\"enum eon_ui::router::Route\">Route</a><div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,</div>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
